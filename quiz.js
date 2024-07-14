6dabfa8631ed7eb4429e066bbb4427a7d271cb03
George Ouma <georgetieno@gmail.com>
2024-07-14 19:21:47 +0300

    modified quiz.js

--- quiz.js
+++ quiz.js
@@ -3,13 +3,13 @@ function checkAnswer(){
     let correctAnswer = '4';
 
     // retrieve the users answer
-    let userAswer = document.querySelector('input[name="quiz"]:checked').value;
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
     // accessing the feedback element
     let feedback = document.querySelector('#feedback');
 
     // comparing users answe with the correct one
-    if (correctAnswer === userAswer){
+    if (correctAnswer === userAnswer){
         feedback.textContent = 'Correct! Well done.';
         feedback.style.color = 'green';
     } 
